4f9e27fde5e28a7e8e0df201aaed3026dd988569
Jake Metherell <jake@fluffylogic.com>
2022-07-15 16:19:12 +0100

    Add redirect controller for requests to /id/company/*

---
+++ src/test/java/uk/gov/companieshouse/uri/web/controller/RedirectControllerTest.java
@@ -0,0 +1,68 @@
+package uk.gov.companieshouse.uri.web.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.net.URISyntaxException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Rule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@ExtendWith(MockitoExtension.class)
+class RedirectControllerTest {
+
+    private static final String COMPANY_NUMBER = "12345678";
+    private static final String INVALID_REDIRECT_URI_PREFIX = "xttp://test-server/doc/company";
+    private static final String VALID_REDIRECT_URI_PREFIX = "http://test-server/doc/company";
+    
+    private RedirectController testRedirectController;
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+    
+    @Mock
+    private HttpServletRequest request;
+    
+    @Mock
+    private HttpServletResponse response;
+
+    @BeforeEach
+    protected void setUp() {
+        testRedirectController = new RedirectController();
+    }
+
+    @Test
+    void redirectWithValidRedirectPrefix() throws URISyntaxException {     
+        ReflectionTestUtils.setField(testRedirectController, "redirectURIPrefix", VALID_REDIRECT_URI_PREFIX);
+        when(request.getRequestURI()).thenReturn("/id/company/" + COMPANY_NUMBER);
+        
+        ResponseEntity<String> responseEntity = testRedirectController.redirect(request, response);
+        
+        assertEquals(HttpStatus.SEE_OTHER, responseEntity.getStatusCode());
+        
+        HttpHeaders headers = responseEntity.getHeaders();
+        String locationHeader = headers.get("location").get(0);
+        assertEquals(VALID_REDIRECT_URI_PREFIX + "/" + COMPANY_NUMBER, locationHeader);
+    }
+    
+    @Test
+    void redirectWithInValidRedirectPrefix() throws URISyntaxException {     
+        ReflectionTestUtils.setField(testRedirectController, "redirectURIPrefix", INVALID_REDIRECT_URI_PREFIX);
+        when(request.getRequestURI()).thenReturn("/id/company/" + COMPANY_NUMBER);
+        
+        exception.expect(URISyntaxException.class);
+        testRedirectController.redirect(request, response);
+    }
+}
