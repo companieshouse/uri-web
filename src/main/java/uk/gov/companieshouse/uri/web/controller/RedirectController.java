4168ef2aa31d8cf1aaee71af2daa69dfebaf77a5
zrahman-ch <zrahman@companieshouse.gov.uk>
2024-09-09 10:25:28 +0100

    Java 21 upgrade

--- src/main/java/uk/gov/companieshouse/uri/web/controller/RedirectController.java
+++ src/main/java/uk/gov/companieshouse/uri/web/controller/RedirectController.java
@@ -3,8 +3,8 @@ package uk.gov.companieshouse.uri.web.controller;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
